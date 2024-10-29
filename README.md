478490182ecf69e4cf1781e12711d30556d8b8e0
jayel888 <15644@coderacademy.edu.au>
2024-10-29 19:50:17 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# T3W3- Tuesday
+# JS DOM
+- FInding and reading elements
+- Creating and deleting elements
+- Editing elements
---
+++ scripts/dataList.js
@@ -0,0 +1,18 @@
+// Declare the data to display in the front-end
+let dataArray = [
+    "Frankenstein",
+    "Dracula",
+    "Vampire",
+    "Chucky",
+    "Max Verstappen",
+    "Wednesday",
+    "Voldemort",
+    "T-Rex",
+    "Gremlin Gizmo",
+    "Joker",
+    "Clowns"
+];
+
+dataArray.forEach((costume) => {
+    console.log(costume);
+});
